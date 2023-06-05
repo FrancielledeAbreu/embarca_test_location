175b77b9360393902afd5840c3b37e97ff16153f
francielle <francielledeabreus@gmail.com>
2023-06-05 20:08:12 -0300

    feat: show cities added

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,32 +2,46 @@ class CitiesController < ApplicationController
   before_action :find_state_by_acronym, only: :import
 
   def index
+  end
 
+  def show_imported
+    @imported_cities = City.all
   end
 
   def import
     HTTParty::Basement.default_options.update(verify: false)
 
-    state_sigla = params[:state_sigla].upcase
+    state_sigla = params[:state_sigla].to_s.upcase
 
     response = HTTParty.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/#{state_sigla}/distritos")
     cities_data = response.code == 200 ? JSON.parse(response.body) : []
 
-    save_cities(cities_data)
+    return redirect_to root_path, alert: 'Invalid state!' if state_sigla.blank?
+    return redirect_to root_path, alert: 'Error importing cities!' unless cities_data.present?
 
-    redirect_to root_path, notice: 'Cities imported and saved successfully!'
+    if save_cities(cities_data)
+      redirect_to show_imported_cities_path
+    else
+      redirect_to root_path, alert: 'Error importing cities!'
+    end
   end
 
   private
 
   def find_state_by_acronym
-    @state = State.find_by(acronym: params[:state_sigla].upcase)
+    @state = State.find_by(acronym: params[:state_sigla].to_s.upcase)
     redirect_to root_path, alert: 'State not found' if @state.nil?
   end
 
   def save_cities(cities_data)
-    cities_data.take(20).each do |city_data|
-      city = City.new(name: city_data['nome'], acronym: @state.acronym, country: @state.country, state: @state)
+    random_cities = cities_data.sample(20)
+
+    random_cities.each do |city_data|
+      city = City.find_or_initialize_by(
+        name: city_data['nome'],
+        acronym: @state.acronym,
+        country: @state.country,
+        state: @state)
       city.save if city.valid?
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'cities#index'
   post 'cities/import', to: 'cities#import', as: 'import_cities'
+  get 'cities/show_imported', to: 'cities#show_imported', as: 'show_imported_cities'
 end
